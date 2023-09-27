5f8e564f59607b339ccc1efa4dbae9a72cae7a81
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-09-27 09:11:15 +0200

    maquette_blogvr2_corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("menubtn1").addEventListener("click", function () {
+  var submenu = document.querySelector("#menubtn1 .submenu");
+  submenu.classList.toggle("hide");
+  submenu.classList.toggle("show");
+});
+document.getElementById("menubtn2").addEventListener("click", function () {
+  var submenu = document.querySelector("#menubtn2 .submenu");
+  submenu.classList.toggle("hide");
+  submenu.classList.toggle("show");
+});
+document.getElementById("menubtn3").addEventListener("click", function () {
+  var submenu = document.querySelector("#menubtn3 .submenu");
+  submenu.classList.toggle("hide");
+  submenu.classList.toggle("show");
+});
